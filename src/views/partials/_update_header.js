063368d8176e16cc74927d5c640c95bc03969ff2
Gus Caplan <me@gus.host>
2017-09-04 05:44:29 -0500

    Update _update_header.js

--- src/views/partials/_update_header.js
+++ src/views/partials/_update_header.js
@@ -1,15 +1,17 @@
-var header = document.getElementById('owo');
-
 function choice() {
   return Math.random() > 0.5;
 }
 
+var last = '';
+var header = document.getElementById('owo');
 header.href = '#';
 header.onclick = function() {
   var letter = choice() ? 'O' : 'U';
-  header.innerText = [
+  var current = [
     choice() ? letter : letter.toLowerCase(),
     choice() ? 'W' : 'w',
     choice() ? letter : letter.toLowerCase(),
   ].join('');
-}
+  if (current === last) return header.onclick();
+  header.innerText = last = current;
+};
